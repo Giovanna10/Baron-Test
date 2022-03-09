a6727d40d848f1585ea582801be0a6b6d1df9b3b
Giovanna <radica.giovanna@gmail.com>
2022-03-09 17:04:42 +0100

    Typescript integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <p>Simon Baron Test</p>
-        <p className="App-subtitle">
-          Test your ability to read facial expression
-        </p>
+        <p className="App-subtitle">Reading the Mind in the Eyes</p>
       </header>
     </div>
   );
-}
+};
 
 export default App;
